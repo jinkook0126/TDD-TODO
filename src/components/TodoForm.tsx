ca7214df18174b93269400a3618e7e4632535842
이진국 <jinkook0126@naver.com>
2023-07-05 01:52:09 +0900

    feat: TDD의 시작

---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,6 @@
+export const TodoForm = () => (
+  <form action="">
+    <input type="text" placeholder="할 일을 입력하세요." />
+    <button type="submit">등록하기</button>
+  </form>
+);
---
+++ tests/components/TodoForm.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import { TodoForm } from "@/components/TodoForm";
+
+describe("<TodoFrom />", () => {
+  it("has input and button", () => {
+    render(<TodoForm />);
+    const input = screen.getByPlaceholderText("할 일을 입력하세요.");
+    const button = screen.getByText("등록하기");
+    expect(input).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+  });
+});

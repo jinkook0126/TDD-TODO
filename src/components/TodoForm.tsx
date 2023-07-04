f1257a35c5a8349280c7308a189145c4023d6134
이진국 <jinkook0126@naver.com>
2023-07-05 02:01:17 +0900

    feat: input value change 테스트

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,6 +1,19 @@
-export const TodoForm = () => (
-  <form action="">
-    <input type="text" placeholder="할 일을 입력하세요." />
-    <button type="submit">등록하기</button>
-  </form>
-);
+import React, { useState, ChangeEvent } from "react";
+
+export const TodoForm = () => {
+  const [value, setValue] = useState("");
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+  return (
+    <form action="">
+      <input
+        type="text"
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={handleInput}
+      />
+      <button type="submit">등록하기</button>
+    </form>
+  );
+};
--- tests/components/TodoForm.test.tsx
+++ tests/components/TodoForm.test.tsx
@@ -1,12 +1,24 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { TodoForm } from "@/components/TodoForm";
 
 describe("<TodoFrom />", () => {
   it("has input and button", () => {
     render(<TodoForm />);
-    const input = screen.getByPlaceholderText("할 일을 입력하세요.");
+    const input = screen.getByPlaceholderText("할 일을 입력하세요");
     const button = screen.getByText("등록하기");
     expect(input).toBeInTheDocument();
     expect(button).toBeInTheDocument();
+  });
+
+  it("changes input", () => {
+    render(<TodoForm />);
+    const input = screen.getByPlaceholderText("할 일을 입력하세요");
+    fireEvent.change(input, {
+      target: {
+        value: "TDD-TODO",
+      },
+    });
+
+    expect(input).toHaveAttribute("value", "TDD-TODO");
   });
 });
